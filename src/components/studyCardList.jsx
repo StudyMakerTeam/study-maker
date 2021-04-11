c93fad5e740957bf8fbc90bf3a335d9caa891a8d
GlassK <glassk0724@gmail.com>
2021-04-11 16:27:48 +0900

    feat: 스터디 카드 리스트 구현

---
+++ src/components/studyCardList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import StudyCard from "./studyCard";
+
+const StudyCardList = () => {
+  return (
+    <div className="studyCardList">
+      <StudyCard />
+      <StudyCard />
+      <StudyCard />
+      <StudyCard />
+    </div>
+  );
+};
+
+export default StudyCardList;
