b0aebe25d4454d8e2f493cac9edf085d57139600
GlassK <glassk0724@gmail.com>
2021-04-18 16:35:22 +0900

    Header 및 Footer 함수형 컴포넌트로 변경

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "./app.module.css";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
 import Main from "./components/mainPreview/mainPreview";
 import SignUp from "./components/signUp/signUp";
 
 function App() {
   return (
     <>
+      <Header />
       <SignUp />
+      <Footer />
     </>
   );
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,37 +1,35 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import styles from "./header.module.css";
 
-class Header extends PureComponent {
-  render() {
-    return (
-      <header className={styles.header}>
-        <h2>
-          <a href="#" className={styles.logo}>
-            스터디메이커
-          </a>
-        </h2>
-        <nav className={styles.nav}>
-          <ul className={styles.navList}>
-            <li className={styles.navListItem}>
-              <a href="#" className={styles.navText}>
-                스터디 목록
-              </a>
-            </li>
-            <li className={styles.navListItem}>
-              <a href="#" className={styles.navText}>
-                스터디 만들기
-              </a>
-            </li>
-            <li className={styles.navListItem}>
-              <a href="#" className={styles.navText}>
-                로그인
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h2>
+        <a href="#" id={styles.logo}>
+          스터디메이커
+        </a>
+      </h2>
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li className={styles.navListItem}>
+            <a href="#" className={styles.navText}>
+              스터디 목록
+            </a>
+          </li>
+          <li className={styles.navListItem}>
+            <a href="#" className={styles.navText}>
+              스터디 만들기
+            </a>
+          </li>
+          <li className={styles.navListItem}>
+            <a href="#" className={styles.navText}>
+              로그인
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
