bd30ce453d8e6dd84ce9a154ed597278e1329c06
GlassK <glassk0724@gmail.com>
2021-04-18 16:53:43 +0900

    전체 anchor property 삭제

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,28 +4,12 @@ import styles from "./header.module.css";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h2>
-        <a href="#" id={styles.logo}>
-          스터디메이커
-        </a>
-      </h2>
+      <h2 id={styles.logo}>스터디메이커</h2>
       <nav className={styles.nav}>
         <ul className={styles.navList}>
-          <li className={styles.navListItem}>
-            <a href="#" className={styles.navText}>
-              스터디 목록
-            </a>
-          </li>
-          <li className={styles.navListItem}>
-            <a href="#" className={styles.navText}>
-              스터디 만들기
-            </a>
-          </li>
-          <li className={styles.navListItem}>
-            <a href="#" className={styles.navText}>
-              로그인
-            </a>
-          </li>
+          <li className={styles.navListItem}>스터디 목록</li>
+          <li className={styles.navListItem}>스터디 만들기</li>
+          <li className={styles.navListItem}>로그인</li>
         </ul>
       </nav>
     </header>
