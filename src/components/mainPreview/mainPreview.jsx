41f6df82478d872bb3a134a5210b894df694d89a
GlassK <glassk0724@gmail.com>
2021-04-25 18:34:37 +0900

    MainPage의 이미지 MainPreview로 이동

--- src/components/mainPreview/mainPreview.jsx
+++ src/components/mainPreview/mainPreview.jsx
@@ -6,6 +6,13 @@ import styles from "./mainPreview.module.css";
 const MainPreview = () => {
   return (
     <>
+      <div className={styles.mainImage}>
+        <img
+          className={styles.image}
+          src="/images/mainImage.png"
+          alt="메인 페이지"
+        />
+      </div>
       <div className={styles.container}>
         <div className={styles.category}>
           <div className={styles.categoryTitle}>
--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -3,23 +3,10 @@ import Footer from "../components/footer/footer";
 import Header from "../components/header/header";
 import MainPreview from "../components/mainPreview/mainPreview";
 
-const divStyle = {
-  textAlign: "center",
-};
-
-const imgStyle = {
-  width: "inherit",
-  maxWidth: "67%",
-  height: "auto",
-};
-
 const MainPage = () => {
   return (
     <>
       <Header />
-      <div style={divStyle}>
-        <img src="/images/mainImage.png" alt="메인 페이지" style={imgStyle} />
-      </div>
       <MainPreview />
       <Footer />
     </>
