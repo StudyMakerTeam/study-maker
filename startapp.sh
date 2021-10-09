1ef26fd9a3e266ac4a1bc37c75f5d4e021da5299
Ray <55176622+kdl9405@users.noreply.github.com>
2021-10-09 17:18:46 +0900

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -1,15 +1,15 @@
 # /start.sh
 
-# nvm에 대한 환경변수를 설정하는 것임.
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# # nvm에 대한 환경변수를 설정하는 것임.
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-cd $PROJECT_ROOT
+# cd $PROJECT_ROOT
 
-# 원래 node 프로세스 종료
-sudo kill -9 `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
-nohup npm start >/home/ubuntu/logs 2>&1 </home/ubuntu/errors &
+# # 원래 node 프로세스 종료
+# sudo kill -9 `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
+# nohup npm start >/home/ubuntu/logs 2>&1 </home/ubuntu/errors &
 
 
 
