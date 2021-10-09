413f9445d9fbb280ab65a4997a4b4fd7d4ed34d2
Ray <55176622+kdl9405@users.noreply.github.com>
2021-10-09 15:19:03 +0900

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,1 +1,17 @@
-if [ -d "/home/ubuntu/build" ]; then rm -Rf "/home/ubuntu/build"; fi
+# if [ -d "/home/ubuntu/build" ]; then rm -Rf "/home/ubuntu/build"; fi
+
+cd /home/build
+
+sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g yarn
+
+sudo yarn init
+
+
+
+
+
+
+
+
