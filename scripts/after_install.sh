b7bc99f7e381580f51b375199e4507aa895ec1d5
Ray <55176622+kdl9405@users.noreply.github.com>
2021-10-09 14:36:33 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cd /home/ec2-user/server
-sudo npm install
-sudo npm install --save react react-dom react-scripts react-particles-js
-sudo npm install pm2 -g
+cd /home/buld
+
+sudo yarn init
+
+# sudo npm install
+# sudo npm install --save react react-dom react-scripts react-particles-js
+# sudo npm install pm2 -g
