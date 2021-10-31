7289a50c7f1385d7b4c47bff38175cb16de764c1
Ray <55176622+kdl9405@users.noreply.github.com>
2021-10-31 20:14:56 +0900

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -1,25 +1,22 @@
+# copy
 
-cd /home/ubuntu/build
-sudo yarn
-sudo yarn init -y
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-cd /home/ubuntu/build/src
-sudo yarn build
-sudo yarn start
+cd $PROJECT_ROOT
+
+# 원래 node 프로세스 종료
+sudo kill -9 `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
+nohup yarn start >/home/ubuntu/logs 2>&1 </home/ubuntu/errors &
 
 
 
-# /start.sh
+# cd /home/ubuntu/build
+# sudo yarn
+# sudo yarn init -y
 
-# # nvm에 대한 환경변수를 설정하는 것임.
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-# cd $PROJECT_ROOT
-
-# # 원래 node 프로세스 종료
-# sudo kill -9 `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
-# nohup npm start >/home/ubuntu/logs 2>&1 </home/ubuntu/errors &
-
+# cd /home/ubuntu/build/src
+# sudo yarn build
+# sudo yarn start
 
