808ac00e2715ba50041b0e726ffac27dbdadf703
Ray <55176622+kdl9405@users.noreply.github.com>
2021-10-31 20:54:36 +0900

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -4,12 +4,12 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-cd $PROJECT_ROOT
+cd /home/ubuntu/build
 
 # 원래 node 프로세스 종료
 sudo kill -9 `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
 sudo yarn
-sudo start &
+sudo yarn start &
 disown
 
 
