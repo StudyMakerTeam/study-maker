67b85908761b56739b40a277a2c6681e868aab39
Ray <55176622+kdl9405@users.noreply.github.com>
2021-10-31 20:23:24 +0900

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -8,7 +8,7 @@ cd $PROJECT_ROOT
 
 # 원래 node 프로세스 종료
 sudo kill -9 `ps -ef | grep 'node ./bin/www' | awk '{print $2}'`
-nohup yarn start >/home/ubuntu/logs 2>&1 </home/ubuntu/errors &
+nohup yarn start
 
 
 
