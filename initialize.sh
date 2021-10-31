37acfdef9e2af6047a6c46e49bba4bb296f38a6d
Ray <55176622+kdl9405@users.noreply.github.com>
2021-10-31 19:38:15 +0900

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -5,26 +5,6 @@ cd /home/ubuntu/build
 sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g yarn
-# sudo npm i react-scripts
-# cd /home/ubuntu/build
-
-# # sudo yarn init -y
-
-cd /home/ubuntu/build
-sudo yarn
-
-# cd /home/ubuntu/build
-sudo yarn init -y
-
-# cd /home/ubuntu/build
-# sudo yarn start
 
 
-# study-maker
-# 1.0.0
-# description
-# /study-maker
-# anytime
-# MIT
-# true
 
