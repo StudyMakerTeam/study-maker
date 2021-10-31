f429e486087be2a160007d60c1c3ad83019a5ae3
Ray <55176622+kdl9405@users.noreply.github.com>
2021-10-31 19:00:57 +0900

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,20 +1,20 @@
 # if [ -d "/home/ubuntu/build" ]; then rm -Rf "/home/ubuntu/build"; fi
 
-cd /home/ubuntu/build
-
-sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm install -g yarn
-# sudo npm i react-scripts
 # cd /home/ubuntu/build
 
-# sudo yarn init -y
+# sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+# sudo npm install -g yarn
+# # sudo npm i react-scripts
+# # cd /home/ubuntu/build
+
+# # sudo yarn init -y
 
 cd /home/ubuntu/build
 sudo yarn
 
-cd /home/ubuntu/build
-sudo yarn init -y
+# cd /home/ubuntu/build
+# sudo yarn init -y
 
 cd /home/ubuntu/build
 sudo yarn start
